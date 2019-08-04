01c507131df3316047569ebb7a68b82c61ec20ad
Michele Freitas <michele-braga@hotmail.com>
2019-08-04 10:02:06 +0100

    simulando falha no meio da transacao nao alterando valor no banco

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,13 +28,13 @@ public class Program {
 			st = conn.createStatement(); 
 					
 			//saber quant linhas afetadas. SQL - todo vendedor que pertencer ao department 1 vou atualizar seu salário para 2090		
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE departmentId = 1");		
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2091 WHERE departmentId = 1");		
 			
 			//simular falha no meio da transação.Processamento vai ser interrompido e cair no catch
-			//int x = 1;
-			//if(x < 2) {
-			//	throw new SQLException("Fake error");
-			//}
+			int x = 1;
+			if(x < 2) {
+				throw new SQLException("Fake error");
+			}
 			                             
 										//vendedor depart.2 vai ganhar 3090
 			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE departmentId = 2");
